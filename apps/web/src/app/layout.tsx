f264d3597bd3516ac4db4f534bd45a69b565fc6a
Steve Ruben <stevetuenkam@gmail.com>
2024-12-31 11:35:51 +0100

    remove unused files

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
       <html suppressHydrationWarning lang="en">
         <body className={`${inter.className}`}>
+          {children}
         </body>
       </html>
   );
