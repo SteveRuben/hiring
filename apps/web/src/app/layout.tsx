31c8b4cecad39f90110c227ee72df5a3680616be
FN <fankamnicephor@gmail.com>
2025-03-05 07:47:55 +0100

    feat(Landing page): add landing page

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,29 +1,14 @@
 import '@/styles/globals.css';
 
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
+import type React from 'react';
 
-import { I18nProvider } from '@/components/i18n';
-import Navbar from '@/components/layouts/nav/nav-bar';
-import AppStateProvider from '@/lib/providers/app-state-provider';
-import { QueryProvider } from '@/lib/providers/query-provider';
-import { SoundEffectProvider } from '@/lib/providers/sound-effect-provider';
-import { ToastProvider } from '@/lib/providers/toast-provider';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Sharing rythms - Learn from Expert Developers',
-  description:
-    'Sharing more than idea.\nConnect with expert developers for personalized learning sessions',
+  title: 'Hodysse - Enchères inversées',
+  description: "Découvrez une nouvelle façon d'acheter avec les enchères inversées",
 };
 
 export default function RootLayout({
@@ -32,29 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased min-h-screen bg-background`}
-      >
-        <QueryProvider>
-          {/* <FlaggProvider featureFlags={featureFlags}> */}
-          <AppStateProvider>
-            <SoundEffectProvider
-              soundUrls={{
-                mp3: '/sounds/sounds.mp3',
-                webm: '/sounds/sounds.webm',
-              }}
-            >
-              <I18nProvider>
-                <Navbar />
-                <div className="pt-16">{children}</div>
-                <ToastProvider />
-              </I18nProvider>
-            </SoundEffectProvider>
-          </AppStateProvider>
-          {/*  </FlaggProvider> */}
-        </QueryProvider>
-      </body>
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
